633281c85fafb4f4bcab822fcc1a3b68b9481801
youssef el hadraoui <93286066+yel-hadr@users.noreply.github.com>
2023-09-17 03:44:12 +0100

    fix redir mimo

--- execute_unit/ft_is_builting.c
+++ execute_unit/ft_is_builting.c
@@ -6,7 +6,7 @@
 /*   By: yel-hadr < yel-hadr@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/18 16:44:35 by yel-hadr          #+#    #+#             */
-/*   Updated: 2023/09/16 08:45:15 by yel-hadr         ###   ########.fr       */
+/*   Updated: 2023/09/17 03:31:54 by yel-hadr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,32 @@ int exec_builting(t_cmd *cmd, t_list *envp)
 {
 	int save_stdin;
 	int save_stdout;
+	int status;
 	
+	status = 0;
 	ft_save_fd(&save_stdin, &save_stdout);
+
 	ft_redaraction(cmd->redir_in.file, cmd->redir_in.type);
 	ft_redaraction(cmd->redir_out.file, cmd->redir_out.type);
+
 	if (!ft_strncmp(cmd->args[0], "echo", ft_strlen(cmd->args[0])))
-		return (ft_echo(cmd->args));
+		status = ft_echo(cmd->args);
 	else if (!ft_strncmp(cmd->args[0], "cd", ft_strlen(cmd->args[0])))
-		return (ft_cd(cmd->args, envp));
+		status = ft_cd(cmd->args, envp);
 	else if (!ft_strncmp(cmd->args[0], "pwd", ft_strlen(cmd->args[0])))
-		return (ft_pwd());
+		status = ft_pwd();
 	else if (!ft_strncmp(cmd->args[0], "export", ft_strlen(cmd->args[0])))
-		return (ft_export(cmd->args, envp));
+		status = ft_export(cmd->args, envp);
 	else if (!ft_strncmp(cmd->args[0], "exit", ft_strlen(cmd->args[0])))
-		return (ft_exit(cmd->args));
+		status = ft_exit(cmd->args);
 	else if (!ft_strncmp(cmd->args[0], "env", ft_strlen(cmd->args[0])))
-		return (ft_env(envp));
+		status = ft_env(envp);
 	else if (!ft_strncmp(cmd->args[0], "unset", ft_strlen(cmd->args[0])))
-		return (ft_unset(cmd->args, &envp));
+		status = ft_unset(cmd->args, &envp);
+	else
+		status = 127;
 	ft_restore_fd(save_stdin, save_stdout);
-	return (0);
+	return (status);
 }
 
 int is_builting(char *cmd)
